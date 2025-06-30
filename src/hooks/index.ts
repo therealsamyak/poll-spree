eb8d1fd6a5b9f26935c82b1f211ca8dd330cb19d
therealsamyak <therealsamyak@gmail.com>
2025-06-29 19:36:44 -0700

    removed unused code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,3 @@
 // Export any remaining hooks here
-// The voting logic has been moved directly into the components for better integration
 
-import { useQuery } from "convex/react"
-import { api } from "../../../convex/_generated/api"
-
-export const useUserProfile = (userId: string) => {
-  return useQuery(api.users.getUser, { userId })
-}
+export * from "./useCreatePoll"
